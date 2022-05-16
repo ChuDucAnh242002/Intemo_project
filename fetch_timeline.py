901c510ae550798fe9500ada8ece12208b9e8b8d
ChuDucAnh242002 <anh.chu@tuni.fi>
2022-05-16 14:53:03 +0300

    fetch basic timeline from data base into csv file

--- fetch_timeline.py
+++ fetch_timeline.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from github import Github
 import mysql.connector
 
@@ -15,27 +16,65 @@ url = "https://github.com/ChuDucAnh2402/hello-world.git"
 db = mysql.connector.connect(host="localhost", user=username, password=password, database=database)
 cursor = db.cursor(buffered=True)
 
-check_query = ""
-insert_query = ""
+""" check_query = "SELECT * FROM timeline1"
+update_commit_query = "update timeline1 set commitid=%s, date=%s where issueid = %s"
+update_issue_query = "update timeline1 set title=%s where issueid=%s"
+insert_query = "insert into timeline1(issueid, commitid, commitdate, title) values(%s,%s,%s,%s)"
 
-def insert_gitcommits(repo):
-    """
-    This function takes the repository name as an argument then connects to it. Later, it fetches all the commits(successful ones)
-    and stores it in Commit table.
-    #Input:
-      argument1: repo (str)
-    """
-    g = Github(login_or_token= token)
-    g_repo = g.get_user().get_repo(repo)
-    for commit in g_repo.get_commits():
-        commit_comments = commit.commit.message
+commit_query = "SELECT issue, commitid, date from commit1 order by issue, date"
+issue_query = "SELECT issueid, title from issue1 order by issueid" """
+
+join_query = "select i.issueid, c.commitid, c.date, i.title from issue1 i inner join commit1 c on i.issueid = c.issue order by i.issueid, c.date;"
+insert_query = "insert into timeline1(issueid, commitid, commitdate, title) value(%s, %s, %s, %s)"
+
+def insert_timeline():
+    """ cursor.execute(commit_query)
+    commit_record = cursor.fetchall()
+    for r in commit_record:
         try:
-            print("Commit has commet: {}".format(commit_comments))
+            issue = r[0]
+            commit_id = r[1]
+            commit_date = r[2]
+            data = [issue, commit_id, commit_date, NULL]
+
+            cursor.execute(insert_query, data)
+            print("Row inserted {}".format(cursor.rowcount))
+            db.commit()
         except Exception as e:
             print(e)
-            continue
-    
 
+    cursor.execute(issue_query)
+    issue_record = cursor.fetchall()
+    for r1 in issue_record:
+        try:
+            issue_id = r1[0]
+            issue_title = r1[1]
+            cursor.execute(update_issue_query, (issue_title, issue_id))
+            print("Row updated {} for {}".format(cursor.rowcount, issue_id))
+            db.commit()
+            
+        except Exception as e:
+            print(e)
 
+    db.close()
+    cursor.close() """
+
+    cursor.execute(join_query)
+    commit_record = cursor.fetchall()
+    for r in commit_record:
+        try:
+            issue = r[0]
+            commit_id = r[1]
+            commit_date = r[2]
+            issue_title = r[3]
+            data = [issue, commit_id, commit_date, issue_title]
+            cursor.execute(insert_query, data)
+            print("Row inserted {}".format(cursor.rowcount))
+            db.commit()
+
+        except Exception as e:
+            print(e)
+    db.close()
+    cursor.close() 
 if __name__ == "__main__":
-    insert_gitcommits("hello-world")
+    insert_timeline()
---
+++ timeline_csv.py
@@ -0,0 +1,30 @@
+import csv
+
+import mysql.connector
+
+username = "root"
+password = "Saigon12345"
+database = "intemodb"
+token = "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"
+url = "https://github.com/ChuDucAnh2402/hello-world.git"
+db = mysql.connector.connect(host="localhost", user=username, password=password, database=database)
+cursor = db.cursor(buffered=True)
+query = "select * from timeline1"
+def save_timeline():
+    path = "csv/timeline.csv"
+    f = open(path, "w", encoding="utf-8", newline="")
+    writer = csv.writer(f)
+    header = ['issue#', 'commit_id', 'commit_date', 'title']
+    writer.writerow(header)
+
+    cursor.execute(query)
+    record = cursor.fetchall()
+    for r in record:
+        try:
+            data = [r[0], r[1], r[2], r[3]]
+            writer.writerow(data)
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    save_timeline()
